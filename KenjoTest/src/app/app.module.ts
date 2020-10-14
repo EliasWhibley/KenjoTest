5d42a4bb110c7a3ba7bc6d1dfe6d9300a42ac18f
Elías Huerta <elias.huerta@icloud.com>
2020-10-14 13:26:48 +0200

    Autocomplete de Material

--- KenjoTest/src/app/app.module.ts
+++ KenjoTest/src/app/app.module.ts
@@ -6,6 +6,14 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HomeComponent } from './home/home.component';
 import { HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { MatFormFieldModule } from '@angular/material/form-field';
+
+
+import { MatInputModule } from '@angular/material/input';
 
 @NgModule({
   declarations: [
@@ -16,8 +24,12 @@ import { HttpClientModule } from '@angular/common/http';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    HttpClientModule
-
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatAutocompleteModule,
+    MatFormFieldModule,
+    MatInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- KenjoTest/src/app/home/home.component.ts
+++ KenjoTest/src/app/home/home.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { Observable } from 'rxjs';
+import { DataService } from '../data.service';
+
+import { map, startWith } from 'rxjs/operators';
 
 @Component({
   selector: 'app-home',
@@ -6,10 +11,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  allAlbums: any;
+  searchAlbum = new FormControl();
+  options: string[];
+  filteredOptions: Observable<string[]>;
 
-  constructor() { }
+  constructor(private dataService: DataService) {
 
-  ngOnInit(): void {
   }
 
+  async ngOnInit() {
+    this.allAlbums = await this.dataService.getAllAlbums();
+    this.options = this.allAlbums.map(option => option.title);
+    console.log(this.options);
+    this.filteredOptions = this.searchAlbum.valueChanges
+      .pipe(
+        startWith(''),
+        map(value => this._filter(value))
+      );
+  }
+
+  _filter(value: string): string[] {
+    const filterValue = value.toLowerCase();
+
+    return this.options.filter(option => option.toLowerCase().includes(filterValue));
+  }
 }
